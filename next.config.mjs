12c505e3544c3ab501de5770e5438497a94f5631
yogesh <yogeshkp85@gmail.com>
2026-08-13 20:15:22 +0530

    Bake AWS env variables into Next.js bundle at build time

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,13 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  env: {
+    MY_AWS_REGION: process.env.MY_AWS_REGION || "",
+    MY_AWS_ACCESS_KEY_ID: process.env.MY_AWS_ACCESS_KEY_ID || "",
+    MY_AWS_SECRET_ACCESS_KEY: process.env.MY_AWS_SECRET_ACCESS_KEY || "",
+    S3_BUCKET_NAME: process.env.S3_BUCKET_NAME || "",
+    DYNAMODB_TABLE_NAME: process.env.DYNAMODB_TABLE_NAME || "",
+  }
 }
 
 export default nextConfig
